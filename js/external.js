e89ce2939da19097db0b6a66d7ec75baea2cb775
Mary White <mary.a.white28@gmail.com>
2021-08-12 15:40:20 -0400

    modified the welcome html and created a js folder and file

---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("Hello from external Javascript");
+
+alert("Welcome to my Website!");
+
+var userInput = prompt("What is your favorite color?");
+alert(userInput + " is my favorite color too!");
+
+
+

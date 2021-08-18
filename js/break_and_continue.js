fbcb13ec10800b41b62f3edda002c4ffc34afe2d
Mary White <mary.a.white28@gmail.com>
2021-08-18 13:34:57 -0400

    answered questions 1 to 3, working on the ice cream cone question

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,27 @@
 "use strict";
+var userEntry = prompt("Enter an odd number between 1 & 50.");
+while(true){
+    if(userEntry % 2 != 0 && (userEntry >= 1 && userEntry <= 50)) {
+        break;
+    }
+    userEntry = prompt("Enter an odd number between 1 & 50");
+}
+
+//Use a loop and the continue statement to output all
+// the odd numbers between 1 and 50, except for the number the user entered.
+
+console.log("the user number to skip is " + userEntry);
+
+for (var i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+        if (i == userEntry) {
+            console.log("skipping this one: " + userEntry);
+            continue;
+        } else {
+        console.log("here's an odd number " + i)
+    }
+}
+
+//ice cream cones, you must sell all of your ice cream cones before going home. Use a do-while loop

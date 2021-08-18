452d65bc456c4e72265ebcac991e7482e3852142
Mary White <mary.a.white28@gmail.com>
2021-08-18 17:24:46 -0400

    completed ice cream cone exercise

--- js/while.js
+++ js/while.js
@@ -4,4 +4,22 @@ while (i < 65536) {
     i = i * 2
     console.log(i);
 }
-//ice cream cone question will be here, not the break and continue question...
+
+//ice cream cone question will be here
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("beginning number: I have " + allCones + " cones")
+do{
+    var customerOrder = Math.floor(Math.random() * 5) + 1;
+    console.log("customer number is " + customerOrder)
+    if(allCones >= customerOrder) {
+        allCones = allCones - customerOrder; //subtract customerOrder from allCones and store it in the same variable.
+    } else {
+        console.log("sorry, " + customerOrder + " is greater than " + allCones + " no cone for you!");
+    }
+
+    console.log("new number of cones left " + allCones)
+
+} while(allCones > 0);
+    if(allCones === 0){
+        console.log("yay I sold them all!!!");
+    }

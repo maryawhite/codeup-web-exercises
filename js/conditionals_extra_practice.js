9588990516cfb0bacb4401f0865c4657983b47f5
Mary White <mary.a.white28@gmail.com>
2021-08-22 16:35:31 -0400

    answered the intWithinBounds question

--- js/conditionals_extra_practice.js
+++ js/conditionals_extra_practice.js
@@ -94,9 +94,13 @@ possibleBonus();
  */
 
 function intWithinBounds(n, lower, upper) {
-    // write your code here
+    if(Number.isInteger(n) && n >= lower && n < upper){
+        return true;
+    } else {
+        return false;   //negative numbers -5, -10, -1, swap upper and lower?
+    }
 }
-
+intWithinBounds()
 
 /* Perimeters with a Catch
  * Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.

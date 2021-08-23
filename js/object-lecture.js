ab8953edf208cbc42f2573992723c6916c8989ae
Mary White <mary.a.white28@gmail.com>
2021-08-23 15:25:06 -0400

    finishing up the object lecture in class

--- js/object-lecture.js
+++ js/object-lecture.js
@@ -31,11 +31,22 @@ var myDog = {
 
 // We can call the 'constructor' for an object and use it to create our objects:
 
+//this is an object constructor
+//var myObject1 = new Object();
+//console.log(myObject1);
 
 //More often, we'll use this object literal notation structure as shown above and below:
 
+//var myObject2 = {};
+//console.log(myObject2);
 
 //* TODO: Create a new Object and call it 'myCar' using either way to do so shown above (object instance or object literal)
+
+
+
+var myCar = {};
+
+console.log(typeof myCar);
 
 // That's great! But our car object doesn't have much going on, does it? There's no depth or meat to it - how can we fill our object in?
 
@@ -61,8 +72,11 @@ var myDog = {
 // TODO: Fill in your myCar object with properties of make, model, and year with the correct values. If you don't own a vehicle, fill in the details with your favorite vehicle.
 
 //For me? I'm missing the year for myCar so. .
+myCar.make = "Toyota";
+myCar.model = "Camry";
+myCar.year = "2000";
 
-// console.log(myCar);
+console.log(myCar);
 
 //Now I have a completed example of the myCar object - the model and make were added above in prior examples
 
@@ -76,27 +90,55 @@ var myDog = {
 //We can use dot notation or array notation in similar ways to start accessing these properties
 
 //Let's share some information about our cars :)
-    
+
+//alert("Hey ya'll, I own a " + myCar.make + " " + myCar.model);
 
 //We can tap into our prior understanding of assigning variables thus far and assign a property to a variable:
 
 //What if we wanted to create a more complicated piece of data? Perhaps a list of our cars FEATURES?
 
-
-//Now we could do something like this. . a list of those features in the console
+//for (var i = 0; i < myCar.features.length ; i++); {
+//    console.log("Here's one of my car's features: " + myCar.features[i]);
+//}
 
 // TODO: Add an array of features that your vehicle has!
 
-//I already added an array of features above in the lecture, so I'm good to go.
+myCar.features = ["cassette tape player", "ac", "power windows", "power locks", "sunroof"];
+console.log(myCar.features);
 
 //What about who owns our car? That could be an object itself if we think it through - the owner has a firstName and a lastName at the very least, right?
 //Let's try nesting this object inside of our car object we've been building out
 
 // TODO: Nest a 'carOwner' object of your own into this growing myCar object and console.log the results!
 
+var carOwner = {
+    firstName: "Mary",
+    lastName: "White",
+}
+myCar.owner = carOwner;
+console.log(myCar);
+
+
+
 //An object can also contain within itself a method (function). We can set it up similarly to our properties, but with a value that is the function expression
 
+//var turnMyCarOn = function(){
+//    alert("vroom vroom " + this.make + " " + this.model);
+//};
+//
+//myCar.turnOn = turnMyCarOn //check this...
 
+var turnMyCarOn = function() {
+    alert("vroom vroom " + this.make + " " + this.model);
+};
+
+myCar.turnOn = turnMyCarOn;
+
+//how do you assign the function to the
+//Option A: myCar.turnOn = turnMyCarOn;
+//Option B: myCar.turnOn = function(){alert("vroom vroom" + this.make + " " + this.model);};
+
+//"this" is a reference to the current object or a different object. self referenceing
 /*
 * * * * * * * * * * * * * * * * * * * * * * * * * *
 *                 Arrays of Objects               *
@@ -110,3 +152,18 @@ var myDog = {
 
 
 //One final TODO: A bigger task - set up your own garage and add at least another vehicle into it. Log your garage array to verify it contains the cars you want - once your garage has the right cars, write a loop to access some properties or a method from them!
+
+var sigOtherCar = {
+    make: "Toyota",
+    model: "Tundra",
+    year: 2007,
+    turnOn: turnMyCarOn
+};
+
+sigOtherCar.features = ["manual windows", "manual locks", "ac", "radio"];
+console.log(sigOtherCar);
+
+var myGarage = [myCar, sigOtherCar];
+console.log(myGarage);
+
+myGarage.forEach(function(car){car.turnOn();})

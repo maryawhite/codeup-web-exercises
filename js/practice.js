77d77413617776ea2d23809edae8837bc5e21b22
Mary White <mary.a.white28@gmail.com>
2021-09-02 09:49:19 -0400

    initial file creation to prepare for one-on-one

---
+++ js/practice.js
@@ -0,0 +1,29 @@
+// Define a function named timesFour that takes in an input and multiples that input by four if it is numeric. If the input is not numeric, then return false.
+// timesTwo(0) //0
+// timesTwo(“5”) //20
+// timesTwo(-5) //-20
+// timesTwo(“Texas”) //false
+// timesTwo([2,1,0]) //false
+// timesTwo(true) //false
+// timesTwo(null) //false
+
+
+
+
+// Define a function named convertDaystoHours that takes in one input Days. Return the conversion of the number of days into total hours. If the input is not numeric or a numeric string, convertDaystoHours, should return false.
+// convertDaystoHours(0) //0
+// convertDaystoHours('5') // 120
+// convertDaystoHours('Texas') //false
+// convertDaystoHours([2, 1, 0]) //false
+// convertDaystoHours(true) //false
+// convertDaystoHours(false) //false
+
+
+// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
+// calculateTax(25, 8)				 // “$27.00"
+// calculateTax(10, 12)             // “$11.20”
+// calculateTax(120, 15.5)			 //	“$138.60"
+// calculateTax(10, true)           // false
+// calculateTax([1, 2, 3], 10)      // false
+// calculateTax(“Codeup”, 100)      // false
+// calculateTax()                   // false

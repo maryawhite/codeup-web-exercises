063aabfb3fb8b8fa36a0b2dce2eada5a23623a3c
Mary White <mary.a.white28@gmail.com>
2021-09-02 22:29:05 -0400

    practicing examples from w3 schools 9221

--- js/practice.js
+++ js/practice.js
@@ -1,12 +1,6 @@
 // Define a function named timesFour that takes in an input and multiples
 // that input by four if it is numeric. If the input is not numeric, then return false.
-// timesFour(0) //0
-// timesFour(“5”) //20
-// timesFour(-5) //-20
-// timesFour(“Texas”) //false
-// timesFour([2,1,0]) //false
-// timesFour(true) //false
-// timesFour(null) //false
+
 function timesFour(input){
     if(Array.isArray(input)){   //how to check if your input is an array
         return false;
@@ -17,14 +11,14 @@ function timesFour(input){
     return false;
 }
 
-console.log(timesFour(0));
-console.log(timesFour("5"));
-console.log(timesFour(-5));       //-20
-console.log(timesFour("Texas")); //false
-console.log(timesFour([2,1,0])); //false
-console.log(timesFour(true));   //false
-console.log(timesFour(null));   //false
-console.log(timesFour({}));
+// console.log(timesFour(0));
+// console.log(timesFour("5"));
+// console.log(timesFour(-5));       //-20
+// console.log(timesFour("Texas")); //false
+// console.log(timesFour([2,1,0])); //false
+// console.log(timesFour(true));   //false
+// console.log(timesFour(null));   //false
+// console.log(timesFour({}));
 
 
 // Define a function named convertDaystoHours that takes in one input Days.
@@ -42,24 +36,17 @@ function convertDaystoHours(input){
     }
         return input * 24;
 }
-console.log("--convert days to hours--");
-console.log(convertDaystoHours(0)); //0
-console.log(convertDaystoHours('5')); // 120
-console.log(convertDaystoHours('Texas')); //false
-console.log(convertDaystoHours([2, 1, 0])); //false
-console.log(convertDaystoHours(true)); //false
-console.log(convertDaystoHours(false)); //false
+// console.log("--convert days to hours--");
+// console.log(convertDaystoHours(0)); //0
+// console.log(convertDaystoHours('5')); // 120
+// console.log(convertDaystoHours('Texas')); //false
+// console.log(convertDaystoHours([2, 1, 0])); //false
+// console.log(convertDaystoHours(true)); //false
+// console.log(convertDaystoHours(false)); //false
 
 
 
 // Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
-// calculateTax(25, 8)				 // “$27.00"
-// calculateTax(10, 12)             // “$11.20”
-// calculateTax(120, 15.5)			 //	“$138.60"
-// calculateTax(10, true)           // false
-// calculateTax([1, 2, 3], 10)      // false
-// calculateTax(“Codeup”, 100)      // false
-// calculateTax()                   // false
 
 function calculateTax(totPaid, taxPercent){
     var total = false;    //initialize it to false
@@ -73,42 +60,92 @@ function calculateTax(totPaid, taxPercent){
     }
     return total;
 }
-console.log("--calculate tax--");
-console.log(calculateTax(25, 8));				 // “$27.00"
-console.log(calculateTax(10, 12));            // “$11.20”
-console.log(calculateTax(120, 15.5));			 //	“$138.60"
-console.log(calculateTax(10, true));           // false
-console.log(calculateTax([1, 2, 3], 10));      // false
-console.log(calculateTax("Codeup", 100));      // false
-console.log(calculateTax());                   // false
-console.log(calculateTax(100, "1000"));
-
-
-
-
-
+// console.log("--calculate tax--");
+// console.log(calculateTax(25, 8));				 // “$27.00"
+// console.log(calculateTax(10, 12));            // “$11.20”
+// console.log(calculateTax(120, 15.5));			 //	“$138.60"
+// console.log(calculateTax(10, true));           // false
+// console.log(calculateTax([1, 2, 3], 10));      // false
+// console.log(calculateTax("Codeup", 100));      // false
+// console.log(calculateTax());                   // false
+// console.log(calculateTax(100, "1000"));
 
 
 //in class exercise Thur 9-2-21
 //write a fortune teller function that takes in 4 arguments: future job, future spouse, future location, and
 //future number of kids.
 
-function fortuneTeller(x, y, z, n){
-    console.log("Your future job is: " + x + ". Your future husband is " + z + ". Your future house will be in: " + y + ". You will have " + n + " kids.");
+// function fortuneTeller(x, y, z, n){
+//     console.log("Your future job is: " + x + ". Your future husband is " + z + ". Your future house will be in: " + y + ". You will have " + n + " kids.");
+// }
+//
+// fortuneTeller("computer programmer", "the mountains", "Brad", "1"  );
+// fortuneTeller("computer programmer", "the beach", "George", "2"  );
+// fortuneTeller("computer programmer", "the bunker", "Zac", "3"  );
+//
+// console.log("-- reworking the in class exercise --");
+// //rework the in class exercise:
+// function tellFortune(numChildren, partnerName, geoLocation, jobTitle){
+//     var output = "You will have " + numChildren + " kids, and be married to " + partnerName + " and live in " + geoLocation + ", and you will be a " + jobTitle;
+//     return output;
+// }
+// console.log(tellFortune("3", "Brad,", "the mountains", "computer programmer"));
+// console.log(tellFortune("2", "Tim", "the bunker", "back end developer"));
+
+
+//w3 resource examples
+//1. Write a JavaScript function that reverses a number.
+// Example x = 32243;
+// Expected Output : 34223
+function reverseNum(input){
+    input = input.toString();
+    return input.split("").reverse().join("");
+}
+console.log("--reverse a number--")
+console.log(reverseNum(123));   //321
+console.log(reverseNum(557));   //755
+console.log(reverseNum(102103));   //301201
+console.log(reverseNum("456"));    //654
+
+//2. Write a JavaScript function that checks whether a passed string is palindrome or not?  For the assessment, if the string is numeric return false.
+function isPalindrome(string) {
+    if (string === null || !isNaN(string)) {
+        return false;
+    }
+    string = string.toString();
+    string = string.toLowerCase();
+    var newString = string.split("").reverse().join("");
+    if(newString === string){
+        return true;
+    }
+    return false;
 }
 
-fortuneTeller("computer programmer", "the mountains", "Brad", "1"  );
-fortuneTeller("computer programmer", "the beach", "George", "2"  );
-fortuneTeller("computer programmer", "the bunker", "Zac", "3"  );
-
-console.log("-- reworking the in class exercise --");
-//rework the in class exercise:
-function tellFortune(numChildren, partnerName, geoLocation, jobTitle){
-    var output = "You will have " + numChildren + " kids, and be married to " + partnerName + " and live in " + geoLocation + ", and you will be a " + jobTitle;
-    return output;
-}
-console.log(tellFortune("3", "Brad,", "the mountains", "computer programmer"));
-console.log(tellFortune("2", "Tim", "the bunker", "back end developer"));
+console.log("--palindrome--");
+console.log(isPalindrome("Bob"));   //true
+console.log(isPalindrome("Radar"));  //true
+console.log(isPalindrome("tacocat"));  //true
+console.log(isPalindrome("Mary"));    //false
+console.log(isPalindrome(232));      //false
+console.log(isPalindrome(231));      //false
+console.log(isPalindrome("101"));      //false
+console.log(isPalindrome(null));      //false
 
 
+
+//3. Write a JavaScript function that generates all combinations of a string.
+// Example string : 'dog'
+// Expected Output : d,do,dog,o,og,g
+
+
+
+//4. Write a JavaScript function that returns a passed string with letters in alphabetical order. Go to the editor
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+
+//5. Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. Go to the editor
+// Example string : 'the quick brown fox'
+// Expected Output : 'The Quick Brown Fox '
 

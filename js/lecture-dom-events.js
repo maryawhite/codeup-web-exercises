d0d34a795b6950e4d2e17b868cfea5d77551ca07
Mary White <mary.a.white28@gmail.com>
2021-09-21 15:53:42 -0400

    added another button and event listener

--- js/lecture-dom-events.js
+++ js/lecture-dom-events.js
@@ -1,5 +1,6 @@
 var button1 = document.getElementById("btn1");
 var button2 = document.getElementById("btn2");
+var buttonC = document.getElementById('jsstyle');
 
 // button1.addEventListener('click', function(){
 //     // alert("Hello Quasar");
@@ -23,3 +24,11 @@ button1.addEventListener('click', rebeccaPurple)
 button2.addEventListener('click', function(){
     button1.removeEventListener('click', rebeccaPurple)
 })
+
+var style = function(){
+    var leftPara = document.getElementById("changeStyle");
+    leftPara.style.color = "red";
+    leftPara.style.fontFamily = "Comic Sans MS";
+
+}
+buttonC.addEventListener('click', style);

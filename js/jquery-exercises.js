65ee8efe3eaea7050920f55c73feaaec8789f445
Mary White <mary.a.white28@gmail.com>
2021-09-28 16:53:00 -0400

    hover photo

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -4,18 +4,17 @@ $(document).ready(function() {
 //your code should be inside the document ready function
 $("body").css("padding", "1em");
 $("#note-to-self").css("border","1px solid red").css("color", "darkgrey");
-$("li").css("font-size", "20px");
+// $("li").css("font-size", "20px");
 $(".codeup").css("background-color","yellow");
 var tagline = $("#tagline").html();
     alert(tagline);
 
-// $("img").css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
-$("img").hover(function() {
-    $(this).attr('src','/img/dinosaur.jpg').css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+$(".image").css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+$(".image").hover(function() {
+    $(this).attr('src', '/img/dinosaur.jpg');
     },
     function(){
-    $(this).attr('src','/img/forest.jpg').css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
-
+    $(this).attr('src','/img/forest.jpg');
 });
 
 //change the bg color of the h1 when clicked

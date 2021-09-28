6978c1c6f444486f74a5587c92906274e10502d6
Mary White <mary.a.white28@gmail.com>
2021-09-28 16:13:31 -0400

    finished exercise and added an image with hover event

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -9,6 +9,31 @@ $(".codeup").css("background-color","yellow");
 var tagline = $("#tagline").html();
     alert(tagline);
 
-$("img").css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+// $("img").css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+$("img").hover(function() {
+    $(this).attr('src','/img/dinosaur.jpg').css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+    },
+    function(){
+    $(this).attr('src','/img/forest.jpg').css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
+
+});
+
+//change the bg color of the h1 when clicked
+    $("h1").click(function(){
+        $(this).css("background-color", "black").css("color","red");
+    });
+
+    //make all p have a font size of 18 when dbl clicked
+    $("p").dblclick(function(){
+        $(this).css("font-size", "18px");
+    })
+
+    $("li").hover(function(){
+        $(this).css("background-color", "black").css("color","red");
+        },
+        function(){
+        $(this).css("background-color", "inherit").css("color", "inherit");
+    });
+
 
 });

768b63d3fa1c5ef833767da7d5bdc88723a541df
Mary White <mary.a.white28@gmail.com>
2021-09-28 13:25:56 -0400

    completed the exercise

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -2,6 +2,10 @@
 
 $(document).ready(function() {
 //your code should be inside the document ready function
-$(".codeup").css("border","1px solid red");
+// $(".codeup").css("border","1px solid red");
+$("li").css("font-size", "20px");
+$("h1,p,li").css("background-color","yellow");
+var newAlert = $("h1").html();
+    alert(newAlert);
 
 });

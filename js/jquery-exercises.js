153d2cdff24968ddc406647e73549c96466254e3
Mary White <mary.a.white28@gmail.com>
2021-09-28 14:43:19 -0400

    added a photo with jquery css styling

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -2,10 +2,12 @@
 
 $(document).ready(function() {
 //your code should be inside the document ready function
-// $(".codeup").css("border","1px solid red");
+$("#note-to-self").css("border","1px solid red");
 $("li").css("font-size", "20px");
-$("h1,p,li").css("background-color","yellow");
-var newAlert = $("h1").html();
-    alert(newAlert);
+$(".codeup").css("background-color","yellow");
+var tagline = $("#tagline").html();
+    alert(tagline);
+
+$("img").css("width", "50%").css("margin-left", "auto").css("margin-right", "auto").css("display", "block");
 
 });

93cea34ecc50c540d2b94afdf0e19b36ab7f976b
Mary White <mary.a.white28@gmail.com>
2021-10-04 18:08:36 -0400

    create marker and popup for restaurant

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -9,4 +9,15 @@ var map = new mapboxgl.Map(
         zoom: 10,   //1 would be far away, 20 would be close
         center: [-84.250855892393,33.88110533623017]
     });
-map.setZoom(8);
+map.setZoom(10);
+map.setStyle("mapbox://styles/mapbox/navigation-day-v1")
+
+//create a marker on the exact location of your favorite restaurant
+var mcdMarker = new mapboxgl.Marker({
+    color: "red",
+    setZoom: 20
+    }).setLngLat([-84.25454735410067, 33.88677041009587])
+    .setPopup(new mapboxgl.Popup().setHTML("<p>McDonalds</p>"))
+    .addTo(map);
+
+

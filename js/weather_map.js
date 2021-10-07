2c93b17b0123c8f8adf21b45b606b16ae798c221
Mary White <mary.a.white28@gmail.com>
2021-10-07 15:11:46 -0400

    refactoring my code to use a different api for current conditions and the onecall api for the 5 day forecast

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,15 +1,26 @@
 "use strict"
 
-// $.get("http://api.openweathermap.org/data/2.5/weather", {
-//     APPID: openWeatherAppKey,
-//     // q:     "San Antonio, US", //you can use various keys to get the location, city, id, long/lat etc
-//     lat: 29.423017,
-//     lon: -98.48527,
-//     units: "imperial"       //the default temperature is Kelvin, this changes it to F
-// }).done(function(data) {
-//     console.log(data);
-// });
 //Codeup: When you start exploring a new API, it is important to learn what data is sent back from each request. We can exploring this information using console.log() inside of a .done() handler:
+
+//this is the call current api, note that the url is different and the documentation is different, you can include different keys
+$.get("https://api.openweathermap.org/data/2.5/weather", {
+    APPID: openWeatherAppKey,
+    q:     "Atlanta, GA US", //you can use various keys to get the location, city, id, long/lat etc
+    // lat: 33.8823,
+    // lon: -84.2502,
+    units: "imperial",       //the default temperature is Kelvin, this changes it to F
+}).done(function(data) {
+    console.log(data);
+    //insert code inside the .done
+    $("#weather-main").html("<p>" + data.weather[0].main + "</p>");
+    $("#temperature-value").html(Math.floor(data.main.temp) + "&#176;" + "<span>F</span>");
+    $("#weather-icon").html('<img src="img/weather-icons/' + data.weather[0].icon + '.png" + />');
+    $("#location").html(data.name);
+    $("#wind-speed").html("Current Wind Speed: " + data.wind.speed);
+
+});
+
+
 
 //this is the onecall api, note that the url is different and the documentation is different, you can include different keys
     $.get("https://api.openweathermap.org/data/2.5/onecall", {
@@ -22,11 +33,12 @@
     }).done(function(data) {
         console.log(data);
         //insert code inside the .done
-        $("#weather-main").html("<p>" + data.daily[0].weather[0].main + "</p>");
-        $("#temperature-value").html(Math.floor(data.current.temp) + "&#176;" + "<span>F</span>");
-        $("#weather-icon").html('<img src="img/weather-icons/' + data.daily[0].weather[0].icon + '.png" + />');
-        $("#location").html("Lat: " + data.lat + ",  Lon: " + data.lon);
-        $("#wind-speed").html("Hourly Wind Speed: " + data.hourly[0].wind_speed);
+
+        $("#day1-temp").html("High/Low" + "<br>" +Math.floor(data.daily[0].temp.max) + "&#176;" + "<span>F</span>" + "/" + Math.floor(data.daily[0].temp.min) + "&#176;" + "<span>F</span>");
+        $("#d1-weather-main").html(data.daily[0].weather[0].main);
+        // $("#d1-header").html(new Date((data.daily[0].dt)*1000).toISOString())
+        $("#d1-header").html(new Date((data.daily[0].dt) * 1000).toLocaleString("en-US"));
+
 
 //html = html + "<p>Weather: " + data.daily[0].weather[0].main + "</p>"
         //html = html + "<p>Temp: " + data.daily[0].temp.day + " F</p>"

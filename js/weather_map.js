a69f44f38dca99cd7376185aa3d3e2ac44c89555
Mary White <mary.a.white28@gmail.com>
2021-10-08 22:08:11 -0400

    added the draggable markers, works but not exactly like the curriculum example

--- js/weather_map.js
+++ js/weather_map.js
@@ -41,7 +41,6 @@ function convertDt(dtNum){
     const humanDateFormat = dateObject.toLocaleString("en-US", {weekday: "long", month: "long", day: "numeric", year: "numeric"}) //2019-12-9 10:30:15
     return humanDateFormat;
 }
-//using the function instead of concatenating this in the forloop, new Date((data.daily[i].dt) * 1000).toLocaleString("en-US", {weekday: "long", month: "long", day: "numeric", year: "numeric"})
 
 mapboxgl.accessToken = mapboxApiKey;  //paste your api key
 
@@ -56,61 +55,115 @@ var map = new mapboxgl.Map(
 
 $("#userSearch").click(function(e){
     e.preventDefault();
-    // $("#input-form").css("display", "none");
-
     let query = $("#searchValue").val(); // a string that was input
-    var latitude = "";
-    var longitude = "";
     geocode(query, mapboxApiKey).then(function(results){
         console.log(results);
-        for(var i = 0; i < results.length; i++){
-            if(i === 0){
-                longitude += results[i];
-            } else {
-                latitude += results[i];
-            }
-        }
         var popUp = new mapboxgl.Popup()
             .setHTML("thanks Geocode")
        var myMarker = new mapboxgl.Marker({color: "black", rotation: 45, draggable: true})
             .setLngLat(results)
             .setPopup(popUp)
             .addTo(map)
-        map.setZoom(12)
+        map.setZoom(9)
         map.jumpTo({center: results})
         console.log(query);
 
         //working on draggable marker
-        var results = myMarker.getLngLat();
-        var coordArr = Object.values(results)  //this puts it in an array
-        console.log(results)
-        console.log(coordArr)
+        // var coordArr = Object.values(results)  //this puts the query results in an array
+        // console.log(coordArr)
         function onDragEnd() {
             const lngLat = myMarker.getLngLat();
+            var newLngLat = Object.values(lngLat);
             console.log(lngLat);
-        }
-        myMarker.on('dragend', onDragEnd);
-        //end working on draggable marker
+            var droppedPin = lngLat;
 
-        //put onecall inside of geocode. this is the onecall api
+            console.log(newLngLat);
+            console.log(newLngLat[0])
+            reverseGeocode(lngLat, mapboxApiKey).then(function(dropresults){
+                console.log(dropresults);
+                var city = dropresults.indexOf(",");
+                var newDropresults = dropresults.slice(city +1, dropresults.length).trim();
+                console.log(newDropresults);
+
+
+            //put onecall inside of geocode. this is the onecall api
         $.get("https://api.openweathermap.org/data/2.5/onecall", {
             APPID: openWeatherAppKey,
-            lat: latitude,
-            lon: longitude,
+            lat: newLngLat[1],
+            lon: newLngLat[0],
             units: "imperial",       //the default temperature is Kelvin, this changes it to F
             exclude: "minutely"
         }).done(function(data) {
             console.log(data);
             //insert code inside the .done
-            // var forecast;
             for(var i = 0; i < 5; i++){ //or use data.daily.length for the 8 day forecast from the data
-                var forecast = '<div class="card text-center text-nowrap mb-4"> <p class="card-header w-100 text-wrap">' + convertDt(data.daily[i].dt) + ' </p><div id="card5d" class="card-body w-100 pb-1"><img class="m-auto d-flex flex-column pb-2" src="img/weather-icons/' + data.daily[i].weather[0].icon + '.png"' + '<br> High/Low <br> ' + Math.round(data.daily[i].temp.max) + ' &#176; <span>F</span> / ' + Math.round(data.daily[i].temp.min) + ' &#176; <span>F</span> </div><div class="card-body w-100 pt-0"> ' + data.daily[i].weather[0].main + ' </div></div>'
+                var forecast = '<div class="card text-center mb-4"> <p class="card-header w-100 text-wrap">' + convertDt(data.daily[i].dt) + ' </p><div id="card5d" class="card-body w-100 pb-1"><img class="m-auto d-flex flex-column pb-2" src="img/weather-icons/' + data.daily[i].weather[0].icon + '.png"' + '<br> High/Low <br> ' + Math.round(data.daily[i].temp.max) + ' &#176; <span>F</span> / ' + Math.round(data.daily[i].temp.min) + ' &#176; <span>F</span> </div><div class="card-body w-100 pt-0"> ' + data.daily[i].weather[0].main + '<br>' + newDropresults + ' </div></div>'
                 $("#five-day").append(forecast);
+                // $("#new-five-day").addClass("invisible");
             }
+        });
+
         }); //end of .done for the one call
+        }
+        myMarker.on('dragend', onDragEnd);
+        //end working on draggable marker
 
     });
 });
+
+
+//repeat
+$("#userSearch").click(function(e){
+    e.preventDefault();
+    let query = $("#searchValue").val(); // a string that was input
+    geocode(query, mapboxApiKey).then(function(results){
+        console.log(results);
+        // var popUp = new mapboxgl.Popup()
+        //     .setHTML("thanks Geocode")
+        // var myMarker = new mapboxgl.Marker({color: "blue", rotation: 45, draggable: true})
+        //     .setLngLat(results)
+        //     .setPopup(popUp)
+        //     .addTo(map)
+        // map.setZoom(10)
+        // map.jumpTo({center: results})
+        // console.log(query);
+
+        //working on draggable marker
+        // var coordArr = Object.values(results)  //this puts it in an array
+        // console.log(results)
+        // console.log(coordArr)
+        // function onDragEnd() {
+        //     const lngLat = myMarker.getLngLat();
+        //     var newLngLat = Object.values(lngLat);
+        //     console.log(lngLat);
+        //     console.log(newLngLat);
+        //     console.log(newLngLat[0])
+
+
+            //put onecall inside of geocode. this is the onecall api
+            $.get("https://api.openweathermap.org/data/2.5/onecall", {
+                APPID: openWeatherAppKey,
+                lat: results[1],
+                lon: results[0],
+                units: "imperial",       //the default temperature is Kelvin, this changes it to F
+                exclude: "minutely"
+            }).done(function(data) {
+                console.log(data);
+                //insert code inside the .done
+                for(var i = 0; i < 5; i++){ //or use data.daily.length for the 8 day forecast from the data
+                    var forecast = '<div class="card text-center text-nowrap mb-4"> <p class="card-header w-100 text-wrap">' + convertDt(data.daily[i].dt) + ' </p><div id="card5d" class="card-body w-100 pb-1"><img class="m-auto d-flex flex-column pb-2" src="img/weather-icons/' + data.daily[i].weather[0].icon + '.png"' + '<br> High/Low <br> ' + Math.round(data.daily[i].temp.max) + ' &#176; <span>F</span> / ' + Math.round(data.daily[i].temp.min) + ' &#176; <span>F</span> </div><div class="card-body w-100 pt-0"> ' + data.daily[i].weather[0].main + ' </div></div>'
+                    $("#new-five-day").append(forecast)
+                }
+
+            }); //end of .done for the one call
+        // }
+        // myMarker.on('dragend', onDragEnd);
+        //end working on draggable marker
+
+    });
+});
+
+
 
 //refresh button
 $('#refresh-5day').click(function() {

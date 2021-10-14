aa45e035187e37beb10f5702f8bc93b7c670be60
Mary White <mary.a.white28@gmail.com>
2021-10-14 12:28:45 -0400

    completed 3rd problem, use reduce to get the total years of experience

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,7 +38,7 @@ const users = [
     }
 ];
 
-//Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array
+//2. Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array
 let atLeast3 = users.filter(function(user){
     return user.languages.length >= 3;
 });
@@ -47,7 +47,7 @@ console.log(atLeast3);
 let atLeast3Arrow = users.filter((user) => user.languages.length >= 3);
 console.log(atLeast3Arrow);
 
-//Use .map to create an array of strings where each element is a user's email address
+//3. Use .map to create an array of strings where each element is a user's email address
 let usersEmail = users.map(function(user){
     return user.email;
 });
@@ -56,8 +56,16 @@ console.log(usersEmail);
 let userEmailArrow = users.map((user) => user.email);
 console.log(userEmailArrow);
 
-//Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average
+//4. Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average
+let totalYearsExp = users.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue.yearsOfExperience
+}, 0);
+console.log(totalYearsExp);
 
-//Use .reduce to get the longest email from the list of users.
+let totalYearsExpArrow = users.reduce((accumulator, currentValue) => accumulator + currentValue.yearsOfExperience, 0);
+console.log(totalYearsExpArrow);
 
-//Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+//5. Use .reduce to get the longest email from the list of users.
+
+//6. Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.

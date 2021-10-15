6487b73c84ef614bc18fa9546d8548449cdd9fd7
Mary White <mary.a.white28@gmail.com>
2021-10-14 22:37:26 -0400

    added styling

--- js/promises.js
+++ js/promises.js
@@ -1,4 +1,5 @@
 "use strict"
+$("body").css("background-color", "#CBE1E0");
 
 fetch('https://api.github.com/users', {headers: {'Authorization': 'githubAPI'}})
         .then(function(results){
@@ -8,4 +9,17 @@ fetch('https://api.github.com/users', {headers: {'Authorization': 'githubAPI'}})
                     console.log(resultsObject);
                     return resultsObject.results
                 })
-        })
+        });
+
+//Create a function that accepts a GitHub username,
+// and returns a promise that resolves returning just the date of the last commit that user made.
+
+
+// fetch("https://api.github.com/emojis").then(function(results){
+//     console.log(results)
+//     results.json()
+//         .then((resultsObject) => {
+//             console.log(resultsObject)
+//             return resultsObject.results
+//         });
+// })
